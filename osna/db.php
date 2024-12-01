813ad7d4b60a8c642424005cbca9c5f29f4f1695
Nelwin02 <21-70975@g.batstate-u.edu.ph>
2024-12-01 18:43:58 +0800

    Update db.php

--- osna/db.php
+++ osna/db.php
@@ -1,13 +1,13 @@
 <?php
 // Database connection parameters
 $DB_HOST = 'dpg-ct2lk83qf0us739u2uvg-a.oregon-postgres.render.com';
-$port = '5432';  // Default PostgreSQL port
+
 $DB_NAME = 'opdmsis';
 $DB_USER = 'opdmsis_user';
 $DB_PASSWORD = '3sc6VNaexgXhje2UgoQ4fnvPf8x1KDGG';
 
 // Create connection string
-$conn_string = "host=$DB_HOST port=$port dbname=$DB_NAME user=$DB_USER password=$DB_PASSWORD";
+$conn_string = "host=$DB_HOST  dbname=$DB_NAME user=$DB_USER password=$DB_PASSWORD";
 
 // Establish connection
 $con = pg_connect($conn_string);
