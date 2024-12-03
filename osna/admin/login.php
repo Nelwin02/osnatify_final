70dd42cdd60069e0d579d9de1f5392409634a809
Nelwin02 <21-70975@g.batstate-u.edu.ph>
2024-12-03 13:07:43 +0800

    Update login.php

--- osna/admin/login.php
+++ osna/admin/login.php
@@ -175,7 +175,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     dataType: 'json',
                     success: function(response) {
                         if (response.status === 'success') {
-                            window.location.href = '/admin_dash.php';
+                            window.location.href = '../admin/admin_dash.php';
                         } else {
                             $('#alert-container').html("<div class='alert alert-danger' role='alert'>Invalid username or password!</div>");
                         }
