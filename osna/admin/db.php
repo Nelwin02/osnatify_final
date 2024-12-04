6e4ab30f1792af0ee9d2c9836f306d7bdc13c003
Nelwin02 <21-70975@g.batstate-u.edu.ph>
2024-12-04 10:09:46 +0800

    Update db.php

--- osna/admin/db.php
+++ osna/admin/db.php
@@ -1,5 +1,4 @@
 <?php
-
 // Database connection parameters
 $DB_HOST = 'dpg-ct2lk83qf0us739u2uvg-a.oregon-postgres.render.com';
 $port = '5432';  // Default PostgreSQL port
@@ -20,24 +19,5 @@ if (!$con) {
 ?>
 
 
-<?php
-// Ensure the user is logged in before executing the following code
-if (isset($_GET['id'])) {
-    // Make sure 'id' is provided in the URL
-    $patientId = $_GET['id'];
 
-    // Query the PostgreSQL database
-    $query = pg_query($con, "SELECT * FROM patient_info WHERE id = $patientId");
 
-    if (pg_num_rows($query) > 0) {
-        $row = pg_fetch_assoc($query);
-        echo json_encode($row); // Return the patient data as JSON
-    } else {
-        // If no patient is found, return an error message
-        echo json_encode(['error' => 'No patient found']); 
-    }
-} else {
-    // If 'id' is not set, return an error
-    echo json_encode(['error' => 'Patient ID is required']);
-}
-?>
