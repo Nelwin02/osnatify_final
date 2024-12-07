35fffcd81d5ac589dbd53ed2fc76997efda4fb83
Nelwin02 <21-70975@g.batstate-u.edu.ph>
2024-12-07 16:27:26 +0800

    Update profile.php

--- osna/admin/profile.php
+++ osna/admin/profile.php
@@ -1,4 +1,282 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<!-- Mirrored from dreamguys.co.in/demo/doccure/admin/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 30 Nov 2019 04:12:20 GMT -->
+<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+        <title>Admin Profile</title>
+		
+		<!-- Favicon -->
+		<link rel="shortcut icon" type="image/x-icon" href="assets/img/opd.png">
+		
+		<!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		
+		<!-- Fontawesome CSS -->
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+		
+		<!-- Feathericon CSS -->
+        <link rel="stylesheet" href="assets/css/feathericon.min.css">
+		
+		<link rel="stylesheet" href="assets/plugins/morris/morris.css">
+		
+		<!-- Main CSS -->
+        <link rel="stylesheet" href="assets/css/style.css">
+		
+		<!--[if lt IE 9]>
+			<script src="assets/js/html5shiv.min.js"></script>
+			<script src="assets/js/respond.min.js"></script>
+		<![endif]-->
+		<?php
+  session_start();
+  include 'db.php'; // Include PostgreSQL connection script
+?>
+
 <?php
+    // Check if the session variable 'username' is set
+    if (!isset($_SESSION['username'])) {
+        header("Location: login.php");
+        exit();
+    }
+
+    // Get the username from the session
+    $username = $_SESSION['username'];
+
+    // Use a parameterized query to prevent SQL injection
+    $sql = "SELECT name FROM admin_log WHERE username = $1"; // $1 is a placeholder for parameter binding
+
+    // Prepare the query
+    if ($stmt = pg_prepare($con, "get_name", $sql)) {
+        // Execute the query with the username as the parameter
+        $result = pg_execute($con, "get_name", array($username));
+
+        // Check if the query was successful
+        if ($result) {
+            // Fetch the result
+            $user = pg_fetch_assoc($result);
+            if ($user) {
+                $name = $user['name'];
+            } else {
+                $name = "Unknown";
+            }
+        } else {
+            $name = "Unknown";
+        }
+    } else {
+        // Query preparation failed
+        $name = "Unknown";
+    }
+
+    // Close the PostgreSQL connection
+    pg_close($con);
+?>
+
+    </head>
+    <body>
+	
+		<!-- Main Wrapper -->
+        <div class="main-wrapper">
+		
+			<!-- Header -->
+            <div class="header">
+			
+				<!-- Logo -->
+                <div class="header-left">
+                    <a href="" class="logo">
+						<img src="assets/img/opd.png" alt="Logo">
+					</a>
+					<a href="" class="logo logo-small">
+						<img src="assets/img/opd.png" alt="Logo" width="30" height="30">
+					</a>
+                </div>
+				<!-- /Logo -->
+				
+				<a href="javascript:void(0);" id="toggle_btn">
+					<i class="fe fe-text-align-left"></i>
+				</a>
+				
+				<div class="top-nav-search">
+				
+				</div>
+				
+				<!-- Mobile Menu Toggle -->
+				<a class="mobile_btn" id="mobile_btn">
+					<i class="fa fa-bars"></i>
+				</a>
+				<!-- /Mobile Menu Toggle -->
+				
+				<!-- Header Right Menu -->
+				<ul class="nav user-menu">
+
+			
+					
+					<!-- User Menu -->
+					<li class="nav-item dropdown has-arrow">
+						<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+							<span class="user-img"><img class="rounded-circle" src="./assets/img/profiles/profile.jpeg" width="31" alt="admin"></span>
+						</a>
+						<div class="dropdown-menu">
+							<div class="user-header">
+								<div class="avatar avatar-sm">
+									<img src="./assets/img/profiles/profile.jpeg" alt="User Image" class="avatar-img rounded-circle">
+								</div>
+								<div class="user-text">
+								<h6><?php echo $username; ?></h6>
+									<p class="text-muted mb-0"><?php echo $name; ?></p>
+								</div>
+							</div>
+							<a class="dropdown-item" href="profile.php">My Profile</a>
+							<a class="dropdown-item" href="settings.php">Web Settings</a>
+							<a class="dropdown-item" href="login.php">Logout</a>
+						</div>
+					</li>
+					<!-- /User Menu -->
+					
+				</ul>
+				<!-- /Header Right Menu -->
+				
+            </div>
+			<!-- /Header -->
+			<style>
+				.fa {
+    margin-right: 8px; /* Add some space between the icon and text */
+}
+
+			 </style>
+			
+	<!-- Sidebar -->
+	<div class="sidebar" id="sidebar">
+                <div class="sidebar-inner slimscroll">
+					<div id="sidebar-menu" class="sidebar-menu">
+						<ul>
+							<li class="menu-title"> 
+								<span>Main</span>
+							</li>
+							<li class="active"> 
+								<a href="admin_dash.php"><i class="fe fe-home"></i> <span>Dashboard</span></a>
+							</li>
+							<li class="submenu">
+								<a href="#"><i class="fa fa-wheelchair"></i> <span>Manage Patient</span> <span class="menu-arrow"></span></a>
+								<ul style="display: none;">
+								<li><a href="manage_patient.php"><i class="fa fa-info-circle"></i>Patient Information</a></li>
+								<li><a href="patient.php"><i class="fa fa-stethoscope"></i>Health Records</a></li>
+								</ul>
+								<li class="submenu">
+									<a href="#"><i class="fa fa-user-md"></i> <span>Manage Doctors</span> <span class="menu-arrow"></span></a>
+									<ul style="display: none;">
+									<li><a href="add_doctor.php"><i class="fa fa-user-plus"></i> Add Doctor</a></li>
+									<li><a href="doctor.php"><i class="fa fa-user-md"></i> Doctor List</a></li>
+									</ul>
+								</li>
+								<li class="submenu">
+									<a href="#"><i class="fa fa-user"></i> <span>Manage Clerk</span> <span class="menu-arrow"></span></a>
+									<ul style="display: none;">
+									<li><a href="add_clerk.php"><i class="fa fa-user-plus"></i> Add Clerk</a></li>
+									<li><a href="clerk.php"><i class="fa fa-user-md"></i> Clerk List</a></li>
+									</ul>
+								</li>
+							<li> 
+
+							<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
+
+							<li class="submenu">
+                                <a href="#"><i class="fas fa-bullhorn"></i> <span>Announcement</span> <span class="menu-arrow"></span></a>
+                                <ul style="display: none;">
+                                <li><a href="add_announcement.php"><i class="fas fa-plus"></i>  Add Announcement</a></li>
+                                
+                                </ul>
+                            </li>
+	
+							<li> 
+								<a href="settings.php"><i class="fe fe-vector"></i> <span>Web Settings</span></a>
+							</li>
+							
+							<li> 
+								<a href="profile.php"><i class="fe fe-user-plus"></i> <span>Profile</span></a>
+							</li><br><br><br><br><br><br><br><br>
+							<li> 
+							<a href="login.php"><i class="fa fa-sign-out"></i> <span>Logout</span></a>
+							</li>
+					</div>
+                </div>
+            </div>
+			<!-- /Sidebar -->
+			
+			
+			<!-- Page Wrapper -->
+            <div class="page-wrapper">
+                <div class="content container-fluid">
+					
+					<!-- Page Header -->
+					<div class="page-header">
+						<div class="row">
+							<div class="col">
+								<h3 class="page-title">Profile</h3>
+								<ul class="breadcrumb">
+									<li class="breadcrumb-item"><a href="admin_dash.php">Dashboard</a></li>
+									<li class="breadcrumb-item active">Profile</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- /Page Header -->
+					
+					<div class="row">
+						<div class="col-md-12">
+							<div class="profile-header">
+								<div class="row align-items-center">
+									<div class="col-auto profile-image">
+										<a href="#">
+											<img class="rounded-circle" alt="User Image" src="./assets/img/profiles/profile.jpeg">
+										</a>
+									</div>
+									<div class="col ml-md-n2 profile-user-info">
+										<h4 class="user-name mb-0"><?php echo $username; ?></h4><h6></h6>
+										<h1><?php echo $name; ?></h1>
+										
+									</div>
+									<div class="col-auto profile-btn">
+										
+										
+									</div>
+								</div>
+							</div>
+							<div class="profile-menu">
+								<ul class="nav nav-tabs nav-tabs-solid">
+									<li class="nav-item">
+										<a class="nav-link active" data-toggle="tab" href="#per_details_tab">About</a>
+									</li>
+									<li class="nav-item">
+										<!--<a class="nav-link" data-toggle="tab" href="#password_tab">Password</a>-->
+									</li>
+								</ul>
+							</div>	
+							<div class="tab-content profile-tab-cont">
+								
+								<!-- Personal Details Tab -->
+								<div class="tab-pane fade show active" id="per_details_tab">
+								
+									<!-- Personal Details -->
+									<div class="row">
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title d-flex justify-content-between">
+                    <span>Personal Detail</span>
+                    <a class="edit-link" data-toggle="modal" href="#edit_personal_details">
+                        <i class="fa fa-edit mr-1"></i>Edit
+                    </a>
+                </h5>
+                <div class="row">
+                    <p class="col-sm-2 text-muted text-sm-right mb-0 mb-sm-3">Name</p>
+                    <p class="col-sm-10"><?php echo htmlspecialchars($admin['name'] ?? ''); ?></p>
+                </div>
+            </div>
+        </div>
+
+        <!-- Edit Details Modal -->
+        <?php
         include 'db.php'; // Include the database connection
 
         // Fetch admin details by ID (replace with the actual ID or logic for dynamic assignment)
@@ -96,3 +374,75 @@
         <!-- /Edit Details Modal -->
     </div>
 </div>
+
+
+
+
+
+
+
+
+									
+									<!-- /Personal Details -->
+
+								</div>
+								<!-- /Personal Details Tab -->
+								
+								<!-- Change Password Tab -->
+								<div id="password_tab" class="tab-pane fade">
+								
+									<div class="card">
+										<div class="card-body">
+											<h5 class="card-title">Change Password</h5>
+											<div class="row">
+												<div class="col-md-10 col-lg-6">
+													<form>
+														<div class="form-group">
+															<label>Old Password</label>
+															<input type="password" class="form-control">
+														</div>
+														<div class="form-group">
+															<label>New Password</label>
+															<input type="password" class="form-control">
+														</div>
+														<div class="form-group">
+															<label>Confirm Password</label>
+															<input type="password" class="form-control">
+														</div>
+														<button class="btn btn-primary" type="submit">Save Changes</button>
+													</form>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<!-- /Change Password Tab -->
+								
+							</div>
+						</div>
+					</div>
+				
+				</div>			
+			</div>
+			<!-- /Page Wrapper -->
+		
+        </div>
+		<!-- /Main Wrapper -->
+		
+		<!-- jQuery -->
+        <script src="assets/js/jquery-3.2.1.min.js"></script>
+		
+		<!-- Bootstrap Core JS -->
+        <script src="assets/js/popper.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+		
+		<!-- Slimscroll JS -->
+        <script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+		
+		<!-- Custom JS -->
+		<script  src="assets/js/script.js"></script>
+		
+    </body>
+
+<!-- Mirrored from dreamguys.co.in/demo/doccure/admin/profile.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 30 Nov 2019 04:12:46 GMT -->
+</html>
