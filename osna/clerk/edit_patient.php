659946534e481e906f04248ca5b93cd21c0f8d53
Nelwin02 <21-70975@g.batstate-u.edu.ph>
2024-12-18 06:35:28 +0800

    Update edit_patient.php

--- osna/clerk/edit_patient.php
+++ osna/clerk/edit_patient.php
@@ -146,18 +146,18 @@ if ($stmt) {
 					
 					
 					<!-- User Menu -->
-					<li class="nav-item dropdown has-arrow">
+                                        <li class="nav-item dropdown has-arrow">
 						<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
-							<span class="user-img"><img class="rounded-circle" src="../clerk/Images/<?php echo htmlspecialchars($image); ?>" width="31" alt="clerk"></span>
+							<span class="user-img"> <img src="Images/doctor1.png" class="rounded-circle" width="31" alt="admin"></span>
 						</a>
 						<div class="dropdown-menu">
 							<div class="user-header">
 								<div class="avatar avatar-sm">
-								<span class="user-img"><img class="rounded-circle" src="../clerk/Images/<?php echo htmlspecialchars($image); ?>" width="31" alt="clerk"></span>
+                                <span class="user-img"><img src="Images/doctor1.png" class="rounded-circle" width="31" alt="admin"></span>
 								</div>
 								<div class="user-text">
-								<h6><?php echo htmlspecialchars($clerk_username); ?></h6>
-									<p class="text-muted mb-0"><?php echo htmlspecialchars($name); ?></p>
+								<h6><?php echo $clerk_username; ?></h6>
+									<p class="text-muted mb-0"><?php echo $name; ?></p>
 								</div>
 							</div>
 							<a class="dropdown-item" href="profile.php">My Profile</a>
